707ee42c80bacdd47ba19419ef867e2856d449df
liutaigang(alex) <liutaigang@wezhuiyi.com>
2024-01-17 18:44:54 +0800

    chore: extension 打包为插件

---
+++ packages/extension/README.md
@@ -0,0 +1,6 @@
+# 使用方法
+- 点击 activitybar 上的图标： ![](https://raw.githubusercontent.com/liutaigang/vscode-webview-extension-example/main/documents/assets/activitybar-icon.png)
+- 键入 `ctrl+shift+p` 在指令输入框中输入：panel-view-container.show
+
+# 源码地址
+[https://github.com/liutaigang/vscode-webview-extension-example](https://github.com/liutaigang/vscode-webview-extension-example)
---
+++ packages/extension/rollup.config.mjs
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import json from "@rollup/plugin-json";
+import { readFileSync } from 'fs'
+
+const pkg = JSON.parse(readFileSync('./package.json'))
+
+
+const plugins = [
+    typescript({
+        tsconfig: './tsconfig.json',
+    }),
+    json(),
+    commonjs(),
+    resolve({
+        extensions: ['.ts', '.js']
+    })
+]
+
+export default {
+    input: './src/extension.ts',
+    output: { format: 'cjs', file: pkg.main, },
+    plugins,
+}
