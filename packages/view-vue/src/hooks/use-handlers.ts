4a41eac84019261148bb63defff6fa1d630f579c
liutaigang <liutaigang@wezhuiyi.com>
2025-03-17 11:25:31 +0800

    fix: 打包插件出错 issue #7

--- packages/view-vue/src/hooks/use-handlers.ts
+++ packages/view-vue/src/hooks/use-handlers.ts
@@ -1,5 +1,5 @@
 import { JsonrpcClient, wrap, type MessageReceiver, type MessageSender } from '@jsonrpc-rx/client';
-// import { type HandlersType } from 'extension/handlers-type';
+import { type HandlersType } from 'extension/handlers-type';
 
 const vscodeApi = (window as any).acquireVsCodeApi();
 
@@ -8,5 +8,5 @@ const msgReceiver: MessageReceiver = (handler) => window.addEventListener('messa
 
 const jsonrpcClient = new JsonrpcClient(msgSender, msgReceiver);
 
-// export const useHandlers = () => wrap<HandlersType>(jsonrpcClient);
-export const useHandlers = () => wrap<any>(jsonrpcClient);
+export const useHandlers = () => wrap<HandlersType>(jsonrpcClient);
+// export const useHandlers = () => wrap<any>(jsonrpcClient);
